7ff60f55c4c29345bf69097b280128073643bc29
Peter <pekohl@gmail.com>
2016-01-21 12:56:10 -0500

    Chkpt-7-Services-2 Complete

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -3,8 +3,18 @@
         var SongPlayer = {};
 
         var currentSong = null;
+
+            /**
+             * @desc Buzz object audio file
+             * @type {Object}
+            */
         var currentBuzzObject = null;
 
+            /**
+             * @function setSong
+             * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+             * @param {Object} song
+             */
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
@@ -17,24 +27,24 @@
             });
 
             currentSong = song;
-        };
+            };
 
-        SongPlayer.play = function(song) {
-            if (currentSong !== song) {
-                setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
-                } else if (currentSong === song) {
-                    if (currentBuzzObject.isPaused()) {
+            SongPlayer.play = function(song) {
+                if (currentSong !== song) {
+                    setSong(song);
                     currentBuzzObject.play();
+                    song.playing = true;
+                    } else if (currentSong === song) {
+                        if (currentBuzzObject.isPaused()) {
+                        currentBuzzObject.play();
+                        }
                     }
-                }
-        };
+                };
 
-         SongPlayer.pause = function(song) {
-             currentBuzzObject.pause();
-             song.playing = false;
-         };
+            SongPlayer.pause = function(song) {
+                 currentBuzzObject.pause();
+                 song.playing = false;
+                };
 
         return SongPlayer;
      }
