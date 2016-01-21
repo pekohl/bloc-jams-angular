3b3f26149a612363d9760510abb6ec191c96c59b
Peter <pekohl@gmail.com>
2016-01-21 12:36:30 -0500

    Chkpt-7-Services-2 Fixed mp3 files

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -5,27 +5,30 @@
         var currentSong = null;
         var currentBuzzObject = null;
 
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+
+            currentSong = song;
+        };
+
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
-                if (currentBuzzObject) {
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
+                setSong(song);
+                currentBuzzObject.play();
+                song.playing = true;
                 } else if (currentSong === song) {
                     if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
                     }
                 }
-
-            currentBuzzObject = new buzz.sound(song.audioUrl, {
-             formats: ['mp3'],
-             preload: true
-                });
-
-            currentSong = song;
-
-            currentBuzzObject.play();
-            song.playing = true;
-            }
         };
 
          SongPlayer.pause = function(song) {
